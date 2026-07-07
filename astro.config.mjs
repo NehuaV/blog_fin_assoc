0e81cab49667f789220aadcff93a6f8b4d91395e
Dobri <79987000+NehuaV@users.noreply.github.com>
2026-07-07 04:11:16 +0300

    optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,13 @@
 // @ts-check
-
-import { unified } from "@astrojs/markdown-remark";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
-import rehypeImageZoom from "./src/rehype/rehype-image-zoom.js";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://finassoc.usi.ch",
-  integrations: [
-    mdx(),
-    sitemap(),
-    unified({
-      rehypePlugins: [rehypeImageZoom],
-    }),
-  ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	site: 'https://finassoc.usi.ch',
+	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
